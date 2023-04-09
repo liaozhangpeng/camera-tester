251a29a5f5470c74f737ed9abf4fd3aa2b7d529b
liaozhangpeng <liaozhangpeng@gmail.com>
2023-04-09 23:50:49 +0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'camera-tester';
+export class AppComponent implements OnInit {
+
+  devices: MediaDeviceInfo[] = []
+
+  constructor(){}
+
+  ngOnInit(): void {
+      this.getDevices()
+  }
+
+  getDevices() {
+    if (!navigator.mediaDevices?.enumerateDevices) {
+      console.log("enumerateDevices() not supported.");
+    } else {
+      // List cameras and microphones.
+      navigator.mediaDevices
+        .enumerateDevices()
+        .then((devices) => {
+          this.devices = devices;
+          // devices.forEach((device) => {
+          //   console.log(`${device.kind}: ${device.label} id = ${device.deviceId}`);
+          // });
+        })
+        .catch((err) => {
+          console.error(`${err.name}: ${err.message}`);
+        });
+    }
+  }
 }
